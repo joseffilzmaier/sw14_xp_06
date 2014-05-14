4f79b48aa05188fff86e672930a48bfb60352243
robert-groebl <r.groebl@student.tugraz.at>
2014-05-14 12:30:12 +0200

    [RG,MK]{other} O2 - NAM Server added to repo.

---
+++ NAM-Server/src/com/appsrox/messenger/server/Constants.java
@@ -0,0 +1,14 @@
+package com.appsrox.messenger.server;
+
+public interface Constants {
+
+    /**
+     * Google API Console Browser key.
+     */
+	String API_KEY = "AIzaSyCckiMfBpW3NPkNMuga4jExVMaynTO2V54";
+
+	public static final String SENDER_EMAIL 	= "senderEmail";
+	public static final String RECEIVER_EMAIL 	= "receiverEmail";	
+	public static final String REG_ID 			= "regId";
+	public static final String MESSAGE 			= "message";
+}
